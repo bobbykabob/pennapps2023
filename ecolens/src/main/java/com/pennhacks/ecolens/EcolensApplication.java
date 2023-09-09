3a430fee9857292c41cf44cb5d851e1135daa28e
Oscar Chen <oscar1149412@gmail.com>
2023-09-09 09:51:26 -0400

    Spring Boot Java initial commit

---
+++ ecolens/src/main/java/com/pennhacks/ecolens/EcolensApplication.java
@@ -0,0 +1,13 @@
+package com.pennhacks.ecolens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcolensApplication.class, args);
+	}
+
+}
---
+++ ecolens/src/test/java/com/pennhacks/ecolens/EcolensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pennhacks.ecolens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcolensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
