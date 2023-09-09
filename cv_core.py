7bbbe6d039d56bea0c99821c1798b392a9fa8ad2
Harris Song <songharris2006@gmail.com>
2023-09-09 14:02:53 -0400

    changing yolo -> roboflow

--- cv_core.py
+++ cv_core.py
@@ -7,30 +7,42 @@ import numpy as np
 from ultralytics import YOLO
 import json
 import os
+from roboflow import Roboflow
 
 class CV_Core:
     #call once
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
         cwd = os.getcwd()
-
-        self.model = YOLO(cwd + "/runs/detect/yolov8n_v8_50e3/weights/best.pt", "v8")
+        rf = Roboflow(api_key="mIvPWCB2jGHVELOKW2NA")
+        project = rf.workspace().project("ecolens")
+        self.model = project.version(2).model
 
         # show video through opencv library
         self.show_video = True
     #call every loop
     def update(self):
         ret, frame = self.cap.read()
-
+        cv2.imwrite('temp.jpg', frame)
+        self.model.predict('temp.jpg', confidence=75, overlap=30).save("prediction.jpg")
+        detection_output = self.model.predict('temp.jpg', confidence=50, overlap=30)
 
         if self.show_video:
             self.show_frame(frame)
-        dictionary = {"item_name": "monkey",
-                      "confidence": 0.9
-        }
+        dictionary = {}
 
-        detection_output = self.model.predict(source=0, conf=0.3, show=True)
         print(detection_output)
+
+        if detection_output is not [] or detection_output is not None:
+            detection_output_json = detection_output.json()
+            detection_output_json_predictions = detection_output_json["predictions"]
+            detection_output_json_predictions_list = detection_output_json_predictions[0]
+            print(detection_output_json_predictions)
+            dictionary = {"item_name": detection_output_json_predictions_list["class"],
+                          "confidence": detection_output_json_predictions_list["confidence"]
+                          }
+
+        #print(dictionary)
         # saving json file
         cwd = os.getcwd()
 
