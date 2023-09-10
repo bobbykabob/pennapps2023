759d69255fcf262e65fdd0625d2c676d190be7c7
Oscar Chen <oscar1149412@gmail.com>
2023-09-10 06:35:37 -0400

    new items

--- ecolens/src/main/resources/static/js/dashboard.js
+++ ecolens/src/main/resources/static/js/dashboard.js
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     }
-    
+
 
     function getAllLifetimeTrash() {
         fetch('/trashcan:all?current=false')
